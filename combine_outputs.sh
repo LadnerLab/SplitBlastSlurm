674aff2db58dda91a251bd2ec1129f316f0d0249
Bob Costas <finkzane@gmail.com>
2019-12-09 14:19:07 -0700

    header is now written to the specified file

--- combine_outputs.sh
+++ combine_outputs.sh
@@ -4,14 +4,22 @@
 
 
 temp_dir=$2
-# Header Added to files produced
-echo -e "Query Name\tQuery Length\tSubject Name\tSubject Length\tAlignment Length\tQuery Start\tQuery End\tSubject Start\tSubject End\tHsp Score\tHsp Expect\tHsp Identities\tPercent Match\tNumber of Gaps" >> out.txt
+query="$4"
 
+count=0
 for file in $( ls "$temp_dir" ); do 
     # Add each result to the output file
     num_lines=$( grep -c '' $temp_dir/$file ) 
+
     if [[ $num_lines -gt 0 ]] && [[ $file == *_parsed.txt ]]; then
-       cat "$temp_dir/$file" >> out.txt	
+        out_file="$query".$( echo "$file" | cut -d '.' -f 2- )
+        echo "$out_file"
+        if [[ $count -eq 0 ]]; then
+            echo -e "Query Name\tQuery Length\tSubject Name\tSubject Length\tAlignment Length\tQuery Start\tQuery End\tSubject Start\tSubject End\tHsp Score\tHsp Expect\tHsp Identities\tPercent Match\tNumber of Gaps" >> "$out_file"
+            count=1
+
+        fi
+       cat "$temp_dir/$file" >> "$out_file"
     fi
 done
 
