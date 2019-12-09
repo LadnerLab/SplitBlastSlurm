70ed682ae2e2f2b3e6051f990251d9db8d941ac8
Bob Costas <finkzane@gmail.com>
2019-12-09 14:42:10 -0700

    all profiles now get the MASTER_BLASTER_PATH

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 grep -q "MASTER_BLASTER_PATH" ~/.profile
 
 if [[ $? -ne 0 ]]; then  
-    echo "export MASTER_BLASTER_PATH="$(pwd) >> ~/.profile
-    source ~/.profile
+    for file in $(ls ~/.*profile*); do
+        echo "export MASTER_BLASTER_PATH="$(pwd) >> "$file"
+        source "$file"
+    done
+
     echo MASTER_BLASTER_PATH set to "$MASTER_BLASTER_PATH"
-    echo To realize these changes in the current shell session, do:
-    echo 'source ~/.profile'
+    echo To realize these changes in the current shell session,
+    echo refresh your bash profile.
 
 else
     echo MASTER_BLASTER_PATH is already set to "$MASTER_BLASTER_PATH"
