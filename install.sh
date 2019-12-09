b80af0f85ba815e58ed9ed496316866e5051974f
Bob Costas <finkzane@gmail.com>
2019-12-09 14:46:06 -0700

    MASTER_BLASTER_PATH written to bashrc

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-grep -q "MASTER_BLASTER_PATH" ~/.profile
+grep -q "MASTER_BLASTER_PATH" ~/.bashrc
 
 if [[ $? -ne 0 ]]; then  
-    for file in $(ls ~/.*profile*); do
-        echo "export MASTER_BLASTER_PATH="$(pwd) >> "$file"
-        source "$file"
-    done
+    echo "export MASTER_BLASTER_PATH="$(pwd) >> ~/.bashrc
+    source ~/.bashrc
 
     echo MASTER_BLASTER_PATH set to "$MASTER_BLASTER_PATH"
     echo To realize these changes in the current shell session,
-    echo refresh your bash profile.
+    echo do 'source ~/.bashrc'
 
 else
     echo MASTER_BLASTER_PATH is already set to "$MASTER_BLASTER_PATH"
